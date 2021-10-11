3ff29284727024d2a2c4a7bce8b63f57e9f107f7
Sebastian Lövdahl <slovdahl@hibox.fi>
2021-10-11 16:09:58 +0300

    Initial commit

---
+++ src/main/java/checkstyle/reproducer/UnusedImportBug.java
@@ -0,0 +1,15 @@
+package checkstyle.reproducer;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UnusedImportBug {
+
+    private static final Set<String> FOO;
+    static {
+        FOO = new HashSet<>();
+
+        FOO.add( HashMap[].class.getName() );
+    }
+}
